9e05940dd4acf320984d011b06909676b0743808
Panlichen <myles.l.pan@gmail.com>
2021-03-26 03:26:19 -0600

    sriov-ndp scripts

---
+++ list_VF_info.sh
@@ -0,0 +1,15 @@
+if [ -f "VF_info.txt" ]; then
+  rm VF_info.txt
+fi
+
+lspci | grep Mellanox | awk '{print $1}' > pci_ids.txt
+for pciid in `cat pci_ids.txt`
+do
+    ca_name=`ls /sys/bus/pci/devices/0000:${pciid}/infiniband/`
+    iface=`ls /sys/bus/pci/devices/0000:${pciid}/infiniband/${ca_name}/device/net/`
+    mac=`cat /sys/bus/pci/devices/0000:${pciid}/infiniband/${ca_name}/device/net/${iface}/address`
+    echo "${pciid} ${ca_name} ${iface} ${mac}" >> VF_info_temp.txt
+done
+column -t VF_info_temp.txt > VF_info.txt
+rm pci_ids.txt VF_info_temp.txt
+cat VF_info.txt
