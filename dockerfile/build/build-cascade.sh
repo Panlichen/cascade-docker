6d81911a2d795b2bf22949f500b8ad95da492470
Panlichen <myles.l.pan@gmail.com>
2021-04-16 03:19:22 -0600

    cascade upgrade, choose branch dockerfile

--- dockerfile/build/build-cascade.sh
+++ dockerfile/build/build-cascade.sh
@@ -14,10 +14,10 @@ if [ -d "cascade" ];then
   rm -rf cascade
 fi
 
-git clone --recursive https://github.com/Derecho-Project/cascade.git -b evaluation
+git clone --recursive https://github.com/Derecho-Project/cascade.git -b ${CASCADE_BRANCH}
 cd cascade
-
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
 make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
 make install
+echo CASCADE_BRANCH: ${CASCADE_BRANCH}
