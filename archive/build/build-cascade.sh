1769fa64129d47352b9c164a80439861195195f5
Panlichen <myles.l.pan@gmail.com>
2021-05-28 02:06:03 -0600

    test cascade docker files

--- archive/build/build-cascade.sh
+++ archive/build/build-cascade.sh
@@ -14,10 +14,12 @@ if [ -d "cascade" ];then
   rm -rf cascade
 fi
 
-git clone --recursive https://github.com/Derecho-Project/cascade.git -b ${CASCADE_BRANCH}
+git clone --recursive https://github.com/Panlichen/cascade.git -b ${CASCADE_BRANCH}
 cd cascade
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
-make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
+./build.sh Release USE_VERBS_API
+# mkdir build && cd build
+# cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
+# make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
+cd build-Release
 make install
 echo CASCADE_BRANCH: ${CASCADE_BRANCH}
