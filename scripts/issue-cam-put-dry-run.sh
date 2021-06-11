26844b94b049955e75be5c4a2b921f22ca32b189
Panlichen <myles.l.pan@gmail.com>
2021-06-11 03:26:14 -0400

    adjustment

--- scripts/issue-cam-put-dry-run.sh
+++ scripts/issue-cam-put-dry-run.sh
@@ -4,15 +4,15 @@ for file in `ls cow-fig`
 do
     array=(${file//./ })
     key=${array[0]}
-    echo "$file, $key"
-    if [ $key -eq "001" ]; then
-        sleep 8
-    else
-        sleep 2
-    fi
+    # echo "$file, $key"
+    # if [ $key -eq "001" ]; then
+    #     sleep 8
+    # else
+    #     sleep 2
+    # fi
     cmd="../../dairy_farm_client -f cow-fig/$file -k $key"
     echo $cmd
-    $cmd
+    # $cmd
 done
 
 echo "done issuing put requests"
